83385c3cd4852f410239a6e609ef715aad42e1b1
ZhaoyangHan04 <3319926404@qq.com>
2025-12-08 02:18:02 +0800

    remove default p2f mode

--- script/run_p2f.py
+++ script/run_p2f.py
@@ -83,8 +83,8 @@ async def main() -> None:
     state = Paper2FigureState(request=req, messages=[])
     state.temp_data["round"] = 0
     state.paper_file = 'data/GovBench_ACL_Template.pdf'
-    state.input_type = 'FIGURE'
-    state.fig_draft_path = '/home/ubuntu/liuzhou/hzy/paper2figure/gen_gemini2.png'
+    # state.input_type = 'FIGURE'
+    # state.fig_draft_path = '/home/ubuntu/liuzhou/hzy/paper2figure/gen_gemini2.png'
     state.result_path = f'./outputs/{time.strftime("%Y%m%d_%H%M%S")}'
     os.makedirs(state.result_path, exist_ok=True)
 
